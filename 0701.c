242a3d23bb52ea68972ae8db64721534628e2deb
gangrok <rkdhr585@naver.com>
2023-07-03 10:20:30 +0900

    - test1 - name : gangrok - date : 2023-07-03

---
+++ 0701.c
@@ -0,0 +1,11 @@
+/* 자료형 int
+* 0000 0000 (8bit) -> 양수, 1000 0000 -> 음수, 코딩에서는 음수를 안씀
+unsigned
+signed
+byte: 8bit 이게 2개 또는 4개가 모이면 int, byte만 쓴다. 
+byte <- unsigned char, uint8_t (unsigned 8bit), 0 ~ 255 (unsigned), 0 ~ 0xFF 
+코딩할때는 byte이다.
+hex (16진수) : 0 ~ F 
+
+arm -> 임베디드 시스템,  amd -> 범용 컴퓨터
+요즘 arm으로 넘어감. 우리는 arm만 하자 *윤지석* */
